dc583b144411717526a796253c6f87fc42ebe494
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-04-21 22:55:53 +0700

    1. removed retrain method 2. implement refit method

--- api/models/temperature_predictor.py
+++ api/models/temperature_predictor.py
@@ -5,27 +5,26 @@ import os
 
 
 class TemperaturePredictor(AbstractPredictor):
-    def __init__(self):
+    def __init__(self, location: str):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         model_dir = os.path.join(
             current_dir, "trained_models", "temperature_SARIMA.pkl"
         )
-        self.sarima = self.__get_model(SARIMAXResults.load(model_dir))
+        self.sarima = self.__get_model(SARIMAXResults.load(model_dir), location)
 
     def forecast(self, timestamp):
         return self.sarima.forecast(steps=timestamp)
 
     def refit(self, data):
-        pass
+        return self.sarima.append(data["temperature"])
 
-    def retrain(self, data):
-        pass
-
-    def __get_model(self, saved_model) -> SARIMAXResults:
+    def __get_model(self, saved_model, location: str) -> SARIMAXResults:
         order = saved_model.model.order
         seasonal_order = saved_model.model.seasonal_order
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        data_dir = os.path.join(current_dir, "trained_models", "Kaset_train_data.csv")
+        data_dir = os.path.join(
+            current_dir, "trained_models/data", location + "_train_data.csv"
+        )
         dataset = pd.read_csv(data_dir)
         model = SARIMAX(
             dataset.set_index("ts")["temperature"],
