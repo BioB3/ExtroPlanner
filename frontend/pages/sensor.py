e0467cc63966b341382a128b3b834333a2dbd735
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-04-22 00:40:28 +0700

    add warnings when CO value is high

--- frontend/pages/sensor.py
+++ frontend/pages/sensor.py
@@ -9,11 +9,43 @@ keep_session_state()
 navbar()
 st.title("Latest Sensor Reading")
 
+
+def encode_co(co_value: float):
+    if co_value < 14:
+        return "normal"
+    elif co_value < 25:
+        return "poor"
+    elif co_value < 35:
+        return "unhealthy"
+    else:
+        return "abnormal"
+
+
+def get_warnings_tips(co_value: float):
+    if co_value < 14:
+        return ""
+    elif co_value < 25:
+        return "Moderate contamination risk.\nSymptoms may devolop at this level.\n\
+Leave the area if you feel unwell.\nPeople with heart/lung issues should call their doctor."
+    elif co_value < 35:
+        return "Serious risk.\nSymptoms likely, especially for vulnerable.\n\
+Evacuate as fast as you could even if symptoms are mild or absent."
+    else:
+        return "Extreme risk.\nLife-threatening for all.\n\
+Evacuate immediately and call emergency services."
+
+
 try:
     data = APIFetcher.get_latest_sensor()
     st.markdown(f"Last Updated: {format_datetime(data['ts'])}")
-    df = pd.DataFrame.from_dict([data]).drop(columns=['ts'])
+    df = pd.DataFrame.from_dict([data]).drop(columns=["ts"])
+    df["co"] = df["co"].apply(encode_co)
     st.table(df)
+
+    tips = get_warnings_tips(data["co"])
+    if tips:
+        st.markdown(tips)
+
 except ValueError as e:
     error_message = str(e)
     if "500" in error_message:
